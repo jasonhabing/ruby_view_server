d9a2740f66e58299486f9e2efda7ce9588b42bf8
Jason Habing <jasonhabing@gmail.com>
2013-07-23 20:12:03 -0700

    made name and sports example

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,38 @@
 require 'erb'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+name = "Jason"
+age = 25
+favoritesport = "Hockey"
+male = true
+
+if male == true
+	referas = "He"
+else
+	referas = "She"
+end
+
+if male == true
+	referas2 = "his"
+else
+	referas2 = "her"
+end
+
+
+sports = ['hockey', 'baseball', 'football', 'soccer']
+
+
+
+template = ERB.new "
+
+<p>This person's name is <%= name %></p>
+<p><%= referas %> is <%= age %> and <%= referas2 %> favorite sport is <%= favoritesport %></p>
+<p>By the way, here are some really cool sports:</p>
+<ul>
+<% sports.each do |sport| %> 
+<li><%= sport %></li>
+<% end %>
+</ul>
+
+"
+
 puts template.result(binding)
